b951ef6f7a722d4ba302e7657044a7c0f524cbf0
DrPerkyLegit <DrPerkyFrLegit@gmail.com>
2026-03-14 16:26:15 -0400

    remove more

--- Minecraft.Server/Application.cpp
+++
@@ -1,6 +0,0 @@
-#include "stdafx.h"
-
-int main() {
-	Windows64Minecraft::StartDedicatedServer();
-	return 0;
-}
--- Minecraft.Server/stdafx.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-#include "../Minecraft.Client/Windows64/Windows64_Minecraft.h"
